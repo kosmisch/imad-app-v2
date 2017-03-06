0ef5c98787bbc28c1b942e05266f35a822fefd4a
kosmisch <crystalanku@gmail.com>
2017-03-06 09:09:12 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').Pool;
+
+var config={
+    user:'kosmisch',
+    database: 'kosmisch',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password:process.env.DB_PASSWORD
+};
 
 
 
@@ -78,6 +87,21 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new Pool(config);
+app.get('/test-db', function (req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500);
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+
 
 app.get('/:articleName', function (req,res){
     // articleName==articleOne
