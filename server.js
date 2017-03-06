29aa4e2d57d606eeccd04ef535845fd30abb6dfb
kosmisch <crystalanku@gmail.com>
2017-03-06 09:57:54 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/articles/:articleName', function (req,res){
     // articleName==articleOne
    // var articleName= req.params.articleName;
 // res.send(createTemplate(articles[articleName]));
-pool.query("SELECT * FROM articles WHERE title="+req.params.articleName,function(err,result){
+pool.query("SELECT * FROM articles WHERE title='"+req.params.articleName+"'",function(err,result){
     if(err){
         res.status(500).send(err.toString());
     }
